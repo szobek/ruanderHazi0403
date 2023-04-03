aece23e1941ee5e465fd1ca21819a8214bf565d1
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-04-03 20:34:35 +0200

    javítva az "a" feladat

--- src/ruanderHazi0403/RuanderHazi0403.java
+++ src/ruanderHazi0403/RuanderHazi0403.java
@@ -4,7 +4,26 @@ import java.util.Scanner;
 
 public class RuanderHazi0403 {
 
+    /*
+
+    • A., Kérjünk be a felhasználótól tíz keresztnevet.
+    • A neveket nagybetűs formátumban, vektorban tároljuk!
+    • Listázzuk ki a tömb minden második elemét!
+    • Állapítsuk meg, melyik a leghosszabb név!
+    • Írjuk ki, melyik névben van a legtöbb ’s’ betű!
+    • B., Töltsünk fel egy 4x3-as mátrixot 50 és 100 közötti véletlenszámokkal.
+    • Transzponáljuk egy másik mátrixba!
+    • Listázzuk ki mindkét mátrixot a megfelelő sor/oszlop formátumban!
+    • C., Töltsünk fel egy 3x3-as mátrixot egyjegyű véletlenszámok négyzetre emelt értékeivel.
+    • Listázzuk ki a főátlóban található számokat és összegüket!
+    • Listázzuk ki fordított sorrendben (az utolsó elemtől az elsőig) az eredetileg generált számokat!
+         */
+
     public static void main(String[] args) {
+        aFeladat();
+    }
+
+    static void aFeladat(){
         String[] keresztNevek ={"aaa","nnnnnssssnnnnnnnn","oo","swwww","bbssb","nnnnnn","nnnnnnnn","mmmssssssssssssssmmmm","ppppppppp","zzzzzzzzzzzz"};
 //        tombFeltoltes(keresztNevek);
 //        System.out.println();
@@ -27,12 +46,14 @@ public class RuanderHazi0403 {
     }
 
     static void mindenMasodikNev(String[] keresztNevek){
+        System.out.println("Minden 2.: ");
         for (int i = 0; i < keresztNevek.length; i++) {
             if (i % 2 == 1) {
 
                 System.out.println(keresztNevek[i]);
             }
         }
+        System.out.println();
     }
     static void legHosszabbNev(String[] keresztNevek){
         int nevHossza = keresztNevek[0].length();
@@ -43,10 +64,12 @@ public class RuanderHazi0403 {
                 nevHossza=keresztNevek[i].length();
             }
         }
-        System.out.println("A név: "+keresztNevek[leghosszabbNevIndex]+" a hossza: "+nevHossza+" karakter");
+        System.out.println("A leghosszabb név: "+keresztNevek[leghosszabbNevIndex]+" a hossza: "+nevHossza+" karakter");
     }
 
     static void legtobbSANevben(String[] keresztNevek){
+        int legtobb = 0;
+        String nev = "";
         for (String s : keresztNevek) {
             int tartalmaz = 0;
             for (int j = 0; j < s.length(); j++) {
@@ -55,7 +78,13 @@ public class RuanderHazi0403 {
                 }
             }
             System.out.println("A név: " + s + " " + tartalmaz + "db s-t tartalmaz");
+            if(tartalmaz>legtobb){
+                legtobb=tartalmaz;
+                nev=s;
+            }
+
         }
+        System.out.println( "\na legtöbb \"s\": "+legtobb+", a név: "+nev);
     }
 
 }
